220df5c74c8af8ad66a156b2c796f95097f745b9
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-05 17:16:34 +0200

    functions

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+
+function makeCoffee(sugar, milk) {
+    var instructions = "Boil water, ";
+    instructions += "pour into cup";
+    instructions += "add coffee";
+    instructions += "add " + sugar + "spoons of sugar ";
+    instructions += "add " +milk + "% milk.";
+
+    return instructions
+}
