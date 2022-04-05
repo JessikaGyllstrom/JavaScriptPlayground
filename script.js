1fbb7bf00fa3a618068bafb6ff38000729f56242
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-05 17:29:55 +0200

    arrays

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 console.log("hello world");
 
+//arguments that are passed into the function
+console.log(makeCoffee(2, 20));
 
+// parameters : sugar, milk 
 function makeCoffee(sugar, milk) {
     var instructions = "Boil water, ";
-    instructions += "pour into cup";
-    instructions += "add coffee";
-    instructions += "add " + sugar + "spoons of sugar ";
-    instructions += "add " +milk + "% milk.";
+    instructions += "pour into cup, ";
+    instructions += "add coffee, ";
+    instructions += "add " + sugar + "spoons of sugar, ";
+    instructions += "add " + milk + "% milk.";
 
     return instructions
 }
