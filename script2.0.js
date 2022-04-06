cd1cc100d466ab050d23cde5d7411ceeab76da07
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-06 22:20:13 +0200

    if statements

--- script2.0.js
+++ script2.0.js
@@ -1,5 +1,28 @@
 //Comparison operators
 //Always return a boolean value
-//Nan does not compare
+//Nan does not compare, DONT EVER COMPARE NAN TO NAN...like this =P ...
 
-console.log(NaN == NaN);
+console.log(NaN == NaN);//FALSE
+
+console.log(10 + "10"); // = "1010", 10 is changed into a string
+
+null == undefined; //true
+null === undefined; //false, checks if the datatype is the same
+
+10 == "10"; //true, you can turn an integer into a string, but a string into an integer
+10 === "10";//false, checks if the datatype is the same
+
+//IS NOT EQUAL
+null != null; //false
+null != undefined; //false
+10 != 100; //true
+10 != "10"; //false, 10 is changed into a string
+10 !== "10"; //true
+null !== undefined; //true
+
+var classRegister = ["Yoda", "Anakin", "Chewbacca"];
+
+for(var i = 0; i < classRegister.length; i ++ )
+ {
+    console.log(classRegister[i]);
+}
