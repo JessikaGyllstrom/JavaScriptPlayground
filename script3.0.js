4dc573fecb651a5d9a3d91627980bfaa8fc24eee
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-08 17:45:20 +0200

    leard javascript freecodecamp

---
+++ script3.0.js
@@ -0,0 +1,8 @@
+//declare a variable 
+//any datatype
+//will be accessed in the whole program
+var myName = "Jessika"
+//will be accessed within the scope its created
+let ourName = "freecodecamp";
+//can never be changed
+const pi = 3.14;
