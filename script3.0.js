f5b643f150725b89cd0eb5b57382624f5594e983
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-08 19:54:57 +0200

    working with strings

--- script3.0.js
+++ script3.0.js
@@ -6,3 +6,52 @@ var myName = "Jessika"
 let ourName = "freecodecamp";
 //can never be changed
 const pi = 3.14;
+//declaring a variable
+var a;
+//declaring and assigning
+var b = 2;
+//increment
+myVar++;
+//remainder, gives the remainder of a division
+//often used to find out if a number is even or not
+var remainder = 11 % 3;
+
+a = a + 12;
+//can also be written:
+a += 12;
+
+a = a - 12;
+//can also be written:
+a -= 12;
+
+a = a * 5;
+//can also be written:
+a *= 5;
+
+a = a / 5;
+//can also be written:
+a /= 5;
+
+b = 9 + b;
+//can also be written:
+b += 9;
+
+//when a string contains a quote, use escape-character or ''
+var myStr = "I am a \"quote\" ";
+var myStr = 'I am a "quote" ';
+
+var awsome = "awsome";
+var str = "free code camp is ";
+str += awsome;
+
+var firstName = "Ada";
+firstLetter = firstName[0];
+
+//strings are immutable
+var myStr = "Jello world";
+//you cant change an individual letter like this:
+myStr[0] = "H";
+//this is OK:
+myStr = "Hello World";
+
+
