da5c850404d24b7bd1e3227baaba5c83c5996606
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-10 22:15:26 +0200

    JSON.parse()

--- script6.0.js
+++ script6.0.js
@@ -29,3 +29,12 @@ const evenNumbers = oddNumbers.map(number => {
     return number * 2
 });
 console.log(evenNumbers);
+
+const json = '{"result":true, "count":42}';
+const obj = JSON.parse(json);
+
+console.log(obj.count);
+// expected output: 42
+
+console.log(obj.result);
+// expected output: true
