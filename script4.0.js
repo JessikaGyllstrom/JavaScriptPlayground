e7b627706c2b0982d62e614549ee1f730ff4fbd3
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-10 16:18:35 +0200

    objects

--- script4.0.js
+++ script4.0.js
@@ -15,9 +15,39 @@ console.log("After: " + JSON.stringify(testArr));//2,3,4,5,6
 
 // equa == attempts to convert the both values to compare to a common type
 console.log(3 == '3'); //true
-console.log('3' == 3); //true
+console.log("3" == 3); //true
 
 
 // strict equal === does not attempt to convert the values
 console.log(3 === '3'); //false
-console.log('3' === 3); //false
+console.log("3" === 3); //false
+
+//switch statement
+function caseInSwitch(val) {
+    var answer = "";
+
+    switch(val) {
+        case 1: 
+            answer = "alpha";
+            break;
+        case 2: 
+            answer = "beta";
+            break;
+        case 3: 
+            answer = "gamma"
+            break;
+    }
+    return answer;
+}
+caseInSwitch(1);//alpha
+
+var testObj = {
+    12: "Namath",
+    16: "Montana", 
+    19: "Unitas"
+};
+var playerNumber = 16;
+var player = testObj[playerNumber];
+console.log(playerNumber);//16
+console.log(testObj[playerNumber]);//montana
+console.log(testObj[12]);
