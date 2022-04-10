b9e77633d5c888c2159f9931f8f61083fafa8a13
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-10 18:20:31 +0200

    loops

---
+++ script5.0.js
@@ -0,0 +1,14 @@
+var myArray = [];
+var i = 0;
+while(i < 5 ) {
+    myArray.push(i);
+    i++;
+}
+console.log(myArray);
+
+var ourArray= [];
+
+for (var i = 0; i < 10; i++) {
+    ourArray.push(i + " =) ");
+}
+console.log(ourArray);
