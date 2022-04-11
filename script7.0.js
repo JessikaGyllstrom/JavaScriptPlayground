f00f14b865d20069cbe51f4b2f090e3b66aefc2b
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-11 13:18:06 +0200

    destructuring to pass an object as a functions parameter

--- script7.0.js
+++ script7.0.js
@@ -8,3 +8,21 @@ function removeFirstTwo(list) {
 const arr = removeFirstTwo(source);
 console.log(arr); // 3, 4, 5,  6, 7, 8, 9, 10
 console.log(source); // 1, 2, 3, 4,  5, 6, 7, 8, 9, 10
+
+// destructuring to pass an object as a functions parameter
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34, 
+    median: 34.54, 
+    mode: 23.87, 
+    min: -0.75, 
+    average: 35.85
+};
+const half = (function() {
+    // the max & min from the stats object is passed in as parameters
+    return function half({ max, min }) {
+        return (max + min) / 2.0;
+    };
+})();
+console.log(stats);
+console.log(half(stats));
