d1f4d1bd067ad591a445ced856d4204cb2cd1191
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-11 13:11:14 +0200

    destructuring with the rest-operator

---
+++ script7.0.js
@@ -0,0 +1,10 @@
+// destructuring with the rest-operator
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    // skip the first two values 
+    const [ , , ...arr] = list;
+    return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // 3, 4, 5,  6, 7, 8, 9, 10
+console.log(source); // 1, 2, 3, 4,  5, 6, 7, 8, 9, 10
