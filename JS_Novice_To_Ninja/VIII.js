f498128e81b28c9527006d6979ae23b716362eb1
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-01 17:59:12 +0200

    EVENTS

---
+++ JS_Novice_To_Ninja/VIII.js
@@ -0,0 +1,5 @@
+/* EVENTS
+Events are another part of the DOM and they are what provides the link between 
+the web page and user interactions. Every time a user interacts with a web page, 
+such as clicking on a link, pressing a key, or moving a mouse, an event occurs 
+that our program can detect and then respond to. */ 
